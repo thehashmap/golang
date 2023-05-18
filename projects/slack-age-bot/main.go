1380901647f41c7d30cafa840b56e12169097ceb
NikhilGithubOfficial <nkmnikhil0009@gmail.com>
2023-05-18 11:52:18 +0530

    fix: build error

--- projects/slack-age-bot/main.go
+++ projects/slack-age-bot/main.go
@@ -35,7 +35,7 @@ func main() {
 
 	bot.Command("my yob is <year>", &slacker.CommandDefinition{
 		Description: "yob calculator",
-		Example:     "my yob is 2000",
+		Examples:    []string{"my yob is 2000"},
 		Handler: func(botCtx slacker.BotContext, request slacker.Request, response slacker.ResponseWriter) {
 			year := request.Param("year")
 			yob, err := strconv.Atoi(year)
